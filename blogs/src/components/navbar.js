53385ee425b6e149c9da90a66645c73d5fc64ada
jesu03 <95433671+jesu03@users.noreply.github.com>
2023-08-22 13:04:11 +0530

    navbar commit

--- blogs/src/components/navbar.js
+++ blogs/src/components/navbar.js
@@ -1,11 +1,35 @@
 import React from 'react'
-
-const navbar = () => {
+import './Navbar.css';
+const Navbar = () => {
   return (
-    <div>
-      
+    <div className='nav__content'>
+  <nav class="navbar navbar-expand-lg ">
+  <a class="navbar-brand" href="ee">Blog</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="ee">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="ee">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="ee" >contact us</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
     </div>
   )
 }
 
-export default navbar
+export default Navbar
