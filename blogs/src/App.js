c5a024115f3b8c28859f00238520c58b94584a1c
jesu03 <95433671+jesu03@users.noreply.github.com>
2023-09-05 22:32:17 +0530

    push topcontent class

--- blogs/src/App.js
+++ blogs/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Navbar from './Components/Navbar';
+import Topcontent from './Components/Topcontent';
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Topcontent/>
     </div>
   );
 }
---
+++ blogs/src/components/Topcontent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Topcontent.css';
+const Topcontent = () => {
+  return (
+    <div className='content'>
+      <h2>Launch yout own patrion</h2>
+      <h1>Turn your audience into a your Business</h1>
+    </div>
+  )
+}
+
+export default Topcontent
