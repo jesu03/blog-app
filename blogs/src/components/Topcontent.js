83d9a76d0af13512e2df44e4564b74232905eb89
jesu03 <95433671+jesu03@users.noreply.github.com>
2023-09-06 21:30:39 +0530

    commit

--- blogs/src/components/Topcontent.js
+++ blogs/src/components/Topcontent.js
@@ -5,6 +5,13 @@ const Topcontent = () => {
     <div className='content'>
       <h2>Launch yout own patrion</h2>
       <h1>Turn your audience into a your Business</h1>
+      <div className='content_box'>
+      <img src='www.google.com' alt='not'/>
+      <h1>The Heading</h1>
+      <p>lorem ipsum lano madit reao cuns weai agoi the patric meno latto visdom manto pabro kyo shawl</p>
+      <button>save</button>
+      <button>read more</button>
+      </div>
     </div>
   )
 }
